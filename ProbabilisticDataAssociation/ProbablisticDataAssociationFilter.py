00bb979d8fa143a92af5a92104a665dd892bd613
Shahar <avitan101@gmail.com>
2023-08-13 16:52:16 +0300

    Adding a README.md and starting to build the PDAF class

--- ProbabilisticDataAssociation/ProbablisticDataAssociationFilter.py
+++ ProbabilisticDataAssociation/ProbablisticDataAssociationFilter.py
@@ -1,4 +1,24 @@
+import numpy as np
+
 
 class ProbabilisticDataAssociationFilter:
+    """Implement the PDA filter under the assumption mentioned"""
+
     def __init__(self):
+        pass
+
+    def predict(self):
+        """Predict the future state, measurement and covariance based on a known model"""
+        pass
+
+    def validate(self, measurements):
+        """Create validation region based on the measurement"""
+        # Todo: create measurements as list or set and create validation range
+        pass
+
+    def evaluate_association_probability(self):
+        pass
+
+    def update(self):
+        """Update the state and covariance based on the measurements and innovation"""
         pass
---
+++ README.md
@@ -0,0 +1,6 @@
+# Probabilistic Data Association
+
+## Assumption
+* There is only a single target evolving as a dynamic system.
+* Target tracking was initialized.
+* To be completed.....
