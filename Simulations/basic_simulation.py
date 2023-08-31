03c514482d227e7960e14f46407016c28535c520
Shahar <avitan101@gmail.com>
2023-08-31 12:00:31 +0300

    Removed unnecessary comments

--- Simulations/basic_simulation.py
+++ Simulations/basic_simulation.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from DataGeneration.Target import Target
 from DataGeneration.Clutter import Clutter
-from ProbabilisticDataAssociation.ProbablisticDataAssociationFilter import ProbabilisticDataAssociationFilter
+from ProbabilisticDataAssociation.ProbabilisticDataAssociationFilter import ProbabilisticDataAssociationFilter
 
 if __name__ == '__main__':
     # Initiate parameters
@@ -23,22 +23,22 @@ if __name__ == '__main__':
         'validated measurements': True,
         'clutter': True,
         'updated estimate': False,
-        'covariance': False
+        'covariance': True
     }
 
     dt = 0.1
     target = Target(
-        initial_x=0.0, initial_y=0.0, dt=dt, simulation_duration=100,
+        initial_x=0.0, initial_y=0.0, dt=dt, simulation_duration=40,
         initial_vx=1, initial_vy=2, system_variance=0.01 ** 2
     )
     clutter = Clutter(
-        dist_type='log normal', std=1, clutter_size=2
+        dist_type='uniform', std=10, clutter_size=20
     )
     # Define PDAF parameters
     state_size = 4
     #                 x    y   vx   vy
     initial_state = (0.0, 0.0, 1.0, 2.0)
-    initial_covariance_magnitude = 10
+    initial_covariance_magnitude = 40
     transition_matrix = np.array(
         [[1, 0, dt, 0],
          [0, 1, 0, dt],
@@ -95,8 +95,19 @@ if __name__ == '__main__':
         'measurements': validated_measurements,
         'estimated value': ([x for x, y in log_state], [y for x, y in log_state]),
         'clutter': saved_clutter,
-    }  # 'updated estimate': updated_pdaf
+    }
     if plot_type['static']:
+        if to_plot_or_not_to_plot['covariance']:
+            # Calculate the eigenvalues and eigenvectors
+            factor = len(log_state) // len(log_cov)
+            for i, cov in enumerate(log_cov):
+                eigenvalues, eigenvectors = np.linalg.eig(cov)
+                ell = Ellipse(xy=(log_state[i * factor][0], log_state[i * factor][1]), width=np.sqrt(eigenvalues[0]),
+                              height=np.sqrt(eigenvalues[1]), angle=0,
+                              color='r')
+                ell.set_alpha(0.05)
+                plt.gca().add_artist(ell)
+
         if to_plot_or_not_to_plot['true values']:
             plotter.plot_true_values((x_vec, y_vec))
         if to_plot_or_not_to_plot['clutter']:
@@ -119,50 +130,6 @@ if __name__ == '__main__':
                                                        's': 20,
                                                        'label': 'Updating PDAF',
                                                        'marker': '+'})
-        if to_plot_or_not_to_plot['covariance']:
-            # Calculate the eigenvalues and eigenvectors
-            factor = len(log_state)//len(log_cov)
-            for i, cov in enumerate(log_cov):
-                # if i + 1 == len(log_state):
-                eigenvalues, eigenvectors = np.linalg.eig(cov)
-                # print(eigenvalues[0])
-                ell = Ellipse(xy=(log_state[i*factor][0], log_state[i*factor][1]), width=np.sqrt(eigenvalues[0]), height=np.sqrt(eigenvalues[1]), angle=0,
-                              color='r')
-                ell.set_alpha(0.1)
-                plt.gca().add_artist(ell)
-
-
-            # Plot the ellipse
-            # plt.plot(eigenvectors[0] * eigenvalues[0] + log_state[0][0],
-            #          eigenvectors[1] * eigenvalues[1] + log_state[0][1])
-
-            # Add labels and title
-            # plt.xlabel("x")
-            # plt.ylabel("y")
-            # plt.title("Ellipse Boundary")
-            # plt.show()
-
-            # plotter.plot_covariance(log_cov[0])
-            # Choose a scale factor
-            # scale_factor = 1
-            # # for eigvalues in log_cov:
-            #     # Plot the ellipse
-            # eigvals = log_cov[50][0]
-            # ell = Ellipse(xy=(x_points[50], y_points[50]), width=np.sqrt(eigvals[0]) , height=np.sqrt(eigvals[1]), angle=0,
-            #               color='r')
-            # ell.set_alpha(0.1)
-            # plt.gca().add_artist(ell)
-            # Todo: understand the proper way to plot the cov
-
-            # x_points, y_points = zip(*log_state)
-            # x_cov = [x for x, y in log_cov]
-            # y_cov = [y for x, y in log_cov]
-            # x_point_cov = [point + cov for point, cov in zip(x_points, x_cov)]
-            # y_point_cov = [point - cov for point, cov in zip(y_points, y_cov)]
-            # x_point_cov_minus = [point - cov for point, cov in zip(x_points, x_cov)]
-            # y_point_cov_minus = [point + cov for point, cov in zip(y_points, y_cov)]
-            # plotter.plot_covariance((x_point_cov, y_point_cov))
-            # plotter.plot_covariance((x_point_cov_minus, y_point_cov_minus))
 
         plotter.add_grid()
         plotter.add_labels()
