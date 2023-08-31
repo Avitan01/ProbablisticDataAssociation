7db56addf7da82d2619ca01c6135877a32dae7fc
Shahar <avitan101@gmail.com>
2023-08-31 11:55:03 +0300

    Modified to run all cases

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import subprocess
 
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox
-from scipy import stats
-from Tools.Plotter import Plotter
-import numpy as np
-from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    subprocess.call(['python', 'Simulations\\basic_simulation.py'])
-
+    simulation = input('Chose a simulation to run:\n'
+                       'basic\nlocation initial\nvelocity initial\n'
+                       'Pd\nPg\nmonte carlo\nmeasurement noise\n'
+                       'distribution\n')
+    match simulation.lower().strip():
+        case 'basic':
+            simulation_name = 'basic'
+        case 'location initial':
+            simulation_name = 'location_initial_condition'
+        case 'velocity initial':
+            simulation_name = 'velocity_initial_condition'
+        case 'pd':
+            simulation_name = 'Pd'
+        case 'pg':
+            simulation_name = 'Pg'
+        case 'monte carlo':
+            simulation_name = 'monte_carlo'
+        case 'measurement noise':
+            simulation_name = 'measurement_noise'
+        case 'distribution':
+            simulation_name = 'distribution'
+        case _:
+            simulation_name = 'basic'
+    print(f'{simulation_name} was chosen to run\n')
+    subprocess.call(['python', f'Simulations\\{simulation_name}_simulation.py'])
